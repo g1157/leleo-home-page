d02d231aed51a42d3a70aa1d8b3470291bbe0e2c
g1157 <g1157143860@gmail.com>
2025-04-22 22:15:47 +0800

    something

--- src/config.js
+++ src/config.js
@@ -145,7 +145,8 @@ const config = {
     // {go:"🔍 前往",img:"/img/sunshine.jpg",title:"Project 8",subtitle:"8,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://leleo.top",show:false},
   ],
   
-  statement: ["备案号：XXICP备123456789号","Copyright © 2025 Leleo"],
+  // statement: ["备案号：XXICP备123456789号","Copyright © 2025 Blue_noblue"],
+  statement: ["connect the dots."],
 }
 
 export default config
